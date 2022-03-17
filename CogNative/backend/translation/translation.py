31c189fc6bf3816fdb787e5f99332b4ded74657e
Aref Sadeghi <a.sadeghi@tamu.edu>
2022-03-17 15:56:26 -0500

    commited requirements

--- CogNative/backend/translation/translation.py
+++ CogNative/backend/translation/translation.py
@@ -3,13 +3,15 @@ from googletrans import Translator
 
 class translation:
 
-    def __int__(self):
+    def __init__(self):
         self.translator = Translator()
 
+    # this function requires both source and destination languages
     def translate(self, text, src, des):
         result = self.translator.translate(text, src=src, dest=des)
         return result.text
 
+    # this function auto-detects the source language
     def translate_to(self, text, des):
         result = self.translator.translate(text, dest=des)
         return result.text
