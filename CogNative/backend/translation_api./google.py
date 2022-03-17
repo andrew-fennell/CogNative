27fa139e22240755e96b40c7a8319df99a67517d
Aref-Sadeghi-Gh <98727951+Aref-Sadeghi-Gh@users.noreply.github.com>
2022-03-17 14:00:01 -0500

    Delete CogNative/backend/translation_api. directory

--- CogNative/backend/translation_api./google.py
+++
@@ -1,9 +0,0 @@
-from googletrans import Translator
-
-translator = Translator()
-result = translator.translate('this is a beautiful world. One that we '
-                              'hardly appreciate!', src='en', dest='es')
-
-print(result.src)
-print(result.dest)
-print(result.text)
