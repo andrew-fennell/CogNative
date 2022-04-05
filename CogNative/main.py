0629a416723d6e78504ce3d6341bd8123a359536
Andrew Fennell <andrew.fennell1999@me.com>
2022-04-04 20:10:36 -0500

    Added pathlib and shutil to do file system operations

--- CogNative/main.py
+++ CogNative/main.py
@@ -1,26 +1,25 @@
-import os
+from pathlib import Path
+from shutil import rmtree
 import wave
 
 from models.RTVC.RTVC import RTVC
 
 # INITIALIZE RTVC
-v = RTVC("models/RTVC/saved_models/default")
+#v = RTVC("models/RTVC/saved_models/default")
 
 # SET INPUT AUDIO FILE PATH
-file_path = input("Enter input audio file path:\n")
-assert os.path.exists(file_path), "ERROR: Path not found."
+file_path = Path(input("Enter input audio file path:\n"))
+assert file_path.exists(), "ERROR: Path not found."
+assert file_path.suffix == '.wav', "ERROR: Enter an input .wav file"
 
 # ENTER TEXT
 text = input("Enter text for voice clone:\n")
 assert text[-1] == ".", "ERROR: Punctuation missing."
 
 # OUTPUT FILE PATH
-output_path = input("Enter output audio path:\n")
-if "/" in output_path:
-    output_temp = output_path.split("/")
-    output_temp.pop(-1)
-    output_temp = "/".join(output_temp)
-    assert os.path.exists(output_temp), "ERROR: Path not found."
+output_path = Path(input("Enter output audio path:\n"))
+assert output_path.parent.exists(), "ERROR: Directory not found."
+assert output_path.suffix == '.wav', "ERROR: Enter an output .wav file"
 
 # ENCODE (sometimes this takes time, so it is after inputs)
 v.encode_voice(file_path)
@@ -29,13 +28,13 @@ v.encode_voice(file_path)
 input_subs = text.split('. ')
 
 # JOIN <span> SENTENCES TO BE SYNTHESIZED TOGETHER
-span = 3
+span = 2
 input_subs = [". ".join(input_subs[i:i+span]) for i in range(0, len(input_subs), span)]
 
 # OUTPUT AUDIO FILE PATHS
-temp_output_path = 'temp'
-if not os.path.exists(temp_output_path):
-    os.mkdir(temp_output_path)
+temp_output_path = Path('temp')
+if not temp_output_path.exists():
+    temp_output_path.mkdir()
 out_paths = []
 
 # SYNTHESIZE OUTPUT AUDIO
@@ -54,6 +53,4 @@ with wave.open(output_path, 'wb') as wav_out:
             wav_out.writeframes(wav_in.readframes(wav_in.getnframes()))
 
 # REMOVE ALL TEMP FILES
-for f in os.listdir(temp_output_path):
-    os.remove(os.path.join(temp_output_path, f))
-os.rmdir(temp_output_path)
+rmtree(temp_output_path)
