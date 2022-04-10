d13f02abb36ed5b18e96d06dd06d62a3a18980c2
Andrew Fennell <andrew.fennell1999@me.com>
2022-04-10 12:39:05 -0500

    Added testing for the backend modules (#57)
    * Fixed printing bug; Fixed WindowsPath write bug
    
    * Added CogNative/examples to .gitignore
    
    * Added initial testing for STT and translation modules.
    
    * Edited paths in tests. Tests should be run from the main project directory.
    
    * Create python-app.yml
    
    * Added tests for multiple os and python versions

--- CogNative/backend/modules/STT.py
+++ CogNative/backend/modules/STT.py
@@ -1,6 +1,6 @@
 import speech_recognition as sr
 
-from modules import languages
+from .languages import available_languages
 
 
 
@@ -13,11 +13,11 @@ class STT:
         engine -- Engine used to perform speech recognition
         """
 
-        if source_language not in languages.available_languages.keys():
+        if source_language not in available_languages.keys():
             raise (Exception("Please select a supported language."))
 
         # Variable initialization
-        self.source_language = languages.available_languages[source_language]["stt"]
+        self.source_language = available_languages[source_language]["stt"]
 
         # Speech Recognition initializations
         self.r = sr.Recognizer()
