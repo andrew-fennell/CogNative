cd7e5224f2f5ab6abc3fec6b0159bc6d36a107ff
austin.currington <austin.currington@tamu.edu>
2022-04-14 12:52:06 -0500

    Fix text script syntax and add audio script

---
+++ cmdTestAudio.bat
@@ -0,0 +1,2 @@
+python -m CogNative.main -lang english -sampleAudio CogNative\examples\MatthewM66.wav -synType audio -dialogLang english -dialogueAudio CogNative\examples\BillMaher22.wav -out cmdTestAudio.wav -useExistingEmbed n
+start .\cmdTestAudio.wav
