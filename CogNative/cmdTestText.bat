de6daf0f6a2b1c3657e86fe4b73464ddcbdce8a1
austin.currington <austin.currington@tamu.edu>
2022-04-13 21:45:57 -0500

    revert embed change, add cmd test bat script for windows

---
+++ CogNative/cmdTestText.bat
@@ -0,0 +1,2 @@
+python .\main.py -lang english -sampleAudio examples/MatthewM66.wav -synType text -dialogueText "This is a test of the emergency matthew system." -out cmdTestText.wav -useExistingEmbed y
+start .\cmdTestText.wav
