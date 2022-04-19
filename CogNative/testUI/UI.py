9466cfde49154fb576e7c6d2743bdbf264e554c8
Andrew Fennell <andrew.fennell1999@me.com>
2022-04-19 17:44:01 -0500

    Update CogNative/testUI/UI.py
    Fixed issue with embedding checkbox.

--- CogNative/testUI/UI.py
+++ CogNative/testUI/UI.py
@@ -4,13 +4,14 @@ from tkinter.filedialog import askdirectory, askopenfile
 from time import sleep
 import subprocess
 import threading
+from pathlib import Path
 
 from ..models.RTVC.utils.printing import colorize
 from colorama import Fore
 
 ws = Tk()
 ws.title("CogNative")
-ws.geometry("800x400")
+ws.geometry("1000x600")
 
 data = {
     "input": {
@@ -34,13 +35,34 @@ output_dir = ""
 global run_lock
 run_lock = False
 
-def open_file(file_type, file_name, label=None):
+def open_file(file_type, file_name):
     data[file_type][file_name] = askopenfile(
-        mode="r", filetypes=[("Audio Files", "*wav")]
+        mode="r", filetypes=[("Audio Files", "*wav"), ("Text Files", "*txt")]
     ).name
 
+    if file_name == "audio_to_clone":
+        audio_path_lbl.config(text=data[file_type][file_name])
+    elif file_name == "audio_to_transcribe":
+        data["text"]["text"] = None
+        if Path(data[file_type][file_name]).suffix == ".txt":
+            with open(data[file_type][file_name]) as f:
+                lines = f.readlines()
+            text_from_file = ' '.join(lines)
+            data["text"]["text"] = text_from_file
+            data[file_type][file_name] = None
+        
+        if data[file_type][file_name]:
+            text_to_syn_lbl.config(text=data[file_type][file_name])
+        elif file_name == "audio_to_transcribe" and not data[file_type][file_name]:
+            len_data = len(data["text"]["text"])
+            n_chars = 80 if len_data > 80 else len_data
+            text_to_display = data["text"]["text"][:n_chars]
+            if n_chars == 80:
+                text_to_display += "..."
+            text_to_syn_lbl.config(text=text_to_display)
 
-def save_path(file_type, file_name, label=None):
+
+def save_path(file_type, file_name):
     data[file_type][file_name] = askdirectory()
 
     # Set output file path
@@ -64,16 +86,21 @@ def displayProgressBar():
 
 def cloneVoice():
     data["input"]["use_embedding"] = embed_check.get()
-    data["text"]["text"] = text_synthesize_entry.get()
+
+    text_in_entry = text_synthesize_entry.get()
+    if text_in_entry:
+        data["text"]["text"] = text_in_entry
 
     output_dir = data["output"]["output_dir"]
     data["output"]["output_audio_path"] = f"{output_dir}/{output_name_entry.get()}"
     output_dir_lbl.config(text=data["output"]["output_audio_path"])
 
+    embedding_check = 'y' if data["input"]["use_embedding"] else 'n'
+
     cmd = ['python', '-m', 'CogNative.main',
            '-sampleAudio', data["input"]["audio_to_clone"],
            '-out', data["output"]["output_audio_path"],
-           '-useExistingEmbed', 'y'
+           '-useExistingEmbed', embedding_check
            ]
     
     if data["text"]["audio_to_transcribe"]:
@@ -183,48 +210,61 @@ embed_check_box = Checkbutton(
 embed_check_box.grid(row=0, column=2, pady=20)
 
 # ----- ROW 1 ----- #
-text_lang_lbl = Label(ws, text="Text to synthesize:")
-text_lang_lbl.grid(row=1, column=0, padx=10)
+audio_selected_lbl = Label(ws, text="Voice to clone:")
+audio_selected_lbl.grid(row=1, column=0, padx=10, pady=5)
 
-text_synthesize_entry = Entry(ws)
-text_synthesize_entry.grid(row=1, column=1, pady=20)
-
-audio_to_transcribe_lbl = Label(ws, text="OR")
-audio_to_transcribe_lbl.grid(row=1, column=2, padx=10, pady=20)
-
-audio_to_transcribe_btn = Button(
-    ws, text="Choose File", command=lambda: open_file("text", "audio_to_transcribe")
-)
-audio_to_transcribe_btn.grid(row=1, column=3, pady=20)
-
+audio_path_lbl = Label(ws, text="^^^ Select audio file ^^^")
+audio_path_lbl.grid(row=1, column=1, columnspan=2, padx=20, pady=20)
 
 # ----- ROW 2 ----- #
+text_lang_lbl = Label(ws, text="Text to synthesize:")
+text_lang_lbl.grid(row=2, column=0, padx=10)
+
+text_synthesize_entry = Entry(ws)
+text_synthesize_entry.grid(row=2, column=1, columnspan=2, pady=20, ipadx=100)
+
+audio_to_transcribe_lbl = Label(ws, text="OR")
+audio_to_transcribe_lbl.grid(row=2, column=3, padx=15, pady=20)
+
+audio_to_transcribe_btn = Button(
+    ws, text="Choose *.wav or *.txt", command=lambda: open_file("text", "audio_to_transcribe")
+)
+audio_to_transcribe_btn.grid(row=2, column=5, pady=20)
+
+# ----- ROW 3 ----- #
+text_to_syn_intro = Label(ws, text="Text being synthesized:")
+text_to_syn_intro.grid(row=3, column=0, padx=10, pady=5)
+
+text_to_syn_lbl = Label(ws, text="^^^ Either enter text, or select a .wav or .txt file ^^^")
+text_to_syn_lbl.grid(row=3, column=1, columnspan=2, padx=20, pady=20)
+
+# ----- ROW 4 ----- #
 output_path_lbl = Label(ws, text="Output file name:")
-output_path_lbl.grid(row=2, column=0, padx=10, pady=20)
+output_path_lbl.grid(row=4, column=0, padx=10, pady=20)
 
 output_name_entry = Entry(ws)
-output_name_entry.grid(row=2, column=1, pady=20)
+output_name_entry.grid(row=4, column=1, pady=20)
 
 output_path_lbl = Label(ws, text="Select output path:")
-output_path_lbl.grid(row=2, column=2, padx=10, pady=20)
+output_path_lbl.grid(row=4, column=2, padx=10, pady=20)
 
 output_path_btn = Button(
     ws,
     text="Choose Directory",
-    command=lambda: save_path("output", "output_dir", output_dir_lbl),
+    command=lambda: save_path("output", "output_dir"),
 )
-output_path_btn.grid(row=2, column=3, pady=20)
+output_path_btn.grid(row=4, column=3, pady=20)
 
-# ----- ROW 3 ----- #
+# ----- ROW 5 ----- #
 output_selected_lbl = Label(ws, text="Output path:")
-output_selected_lbl.grid(row=3, column=0, padx=10, pady=5)
+output_selected_lbl.grid(row=5, column=0, padx=10, pady=5)
 
 output_dir_lbl = Label(ws, text="")
-output_dir_lbl.grid(row=3, column=1, columnspan=2, padx=20, pady=20)
+output_dir_lbl.grid(row=5, column=1, columnspan=2, padx=20, pady=20)
 
-# ----- ROW 4 ----- #
+# ----- ROW 6 ----- #
 upld = Button(ws, text="Clone voice", command=buttonHandle)
-upld.grid(row=4, columnspan=3, pady=20, padx=20)
+upld.grid(row=6, columnspan=3, pady=20, padx=20, ipadx=25, ipady=12)
 
 # ----- ROW 8 ----- #
 status_lbl = Label(ws, text="").grid(
