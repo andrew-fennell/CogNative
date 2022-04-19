bdc989827800498ba0c1164688a3e8acb560ec5c
austin-currington <austincurr@yahoo.com>
2022-04-19 16:12:08 -0500

    Added help message, added usage for UI and CLI to readme (#84)
    * Update README.md; Added Usage section
    
    * Update README.md; Added help CLI command to Usage section
    
    Co-authored-by: austin.currington <austin.currington@tamu.edu>
    Co-authored-by: Andrew Fennell <andrew.fennell1999@me.com>

--- cmdTestAudio.bat
+++ cmdTestAudio.bat
@@ -1,2 +1,2 @@
-python -m CogNative.main -lang english -sampleAudio CogNative\examples\MatthewM66.wav -synType audio -dialogueLang english -dialogueAudio CogNative\examples\BillMaher22.wav -out cmdTestAudio.wav -useExistingEmbed n
+python -m CogNative.main -sampleAudio CogNative\examples\MatthewM66.wav -synType audio -dialogueAudio CogNative\examples\BillMaher22.wav -out cmdTestAudio.wav -useExistingEmbed n
 start .\cmdTestAudio.wav
